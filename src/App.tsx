626a3145350e917c5586b0374d9fcadddcfefd6a
Sidhheshdongare <sidhheshdesigner@gmail.com>
2025-05-15 18:44:44 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,24 @@
-import { useEffect } from "react";
-// ... other imports
+import { lazy, Suspense } from "react";
+import "./App.css";
+
+const CharacterModel = lazy(() => import("./components/Character"));
+const MainContainer = lazy(() => import("./components/MainContainer"));
+import { LoadingProvider } from "./context/LoadingProvider";
 
 const App = () => {
-  useEffect(() => {
-    if (typeof window !== "undefined" && window.ScrollSmoother) {
-      window.ScrollSmoother.create({
-        smooth: 1.5,
-        effects: true,
-      });
-    }
-  }, []);
-
   return (
-    <LoadingProvider>
-      <Suspense fallback={<div>Loading app...</div>}>
-        <MainContainer>
-          <CharacterModel />
-        </MainContainer>
-      </Suspense>
-    </LoadingProvider>
+    <>
+      <LoadingProvider>
+        <Suspense>
+          <MainContainer>
+            <Suspense>
+              <CharacterModel />
+            </Suspense>
+          </MainContainer>
+        </Suspense>
+      </LoadingProvider>
+    </>
   );
 };
+
+export default App;
